2125c0511489083342867423ef1d4eb2fa7fce21
Bernadete Lima Pimentel <TacyanneBernadete.Pimentel@cencosud.com.br>
2020-05-17 11:56:11 -0300

    Logon

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import {FiLogIn} from 'react-icons/fi';
 
 import './styles.css';
 
-export default function Logon(){
-    return(
-        <h1>Hello Word</h1>
+
+import cheersImg from '../../assets/saude2.png';
+import logoImg from "../../assets/Saude1.png";
+
+export default function Logon() {
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={cheersImg} alt="Logo" />
+
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Sua ID" />
+                    <button className="button" type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color= "#e02041"/>
+                        Não tenho cadastro!
+                        </a>
+                </form>
+            </section>
+            <img src={logoImg} alt="Cheers" />
+        </div>
     );
 }
